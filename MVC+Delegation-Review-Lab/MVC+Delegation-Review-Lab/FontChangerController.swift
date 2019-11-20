595ea8fac9ec3468a00fafecc696f7b14aeda1e3
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-20 16:36:12 -0500

    set objects for seconds VC

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/FontChangerController.swift
@@ -0,0 +1,20 @@
+//
+//  FontChangerController.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Yuliia Engman on 11/20/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class FontChangerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}

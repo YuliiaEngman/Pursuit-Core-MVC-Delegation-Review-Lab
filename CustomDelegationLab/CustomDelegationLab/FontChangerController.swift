a4a0fab45a9fe43acda79f9edc6716fbbdace826
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-27 11:30:15 -0500

    set layout for forst VC

---
+++ CustomDelegationLab/CustomDelegationLab/FontChangerController.swift
@@ -0,0 +1,30 @@
+//
+//  FontChangerController.swift
+//  CustomDelegationLab
+//
+//  Created by Yuliia Engman on 1/27/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class FontChangerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

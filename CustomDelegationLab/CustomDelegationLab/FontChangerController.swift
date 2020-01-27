3a087f092e04866b487bbdf27bf7d9601dfb3068
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-27 11:35:02 -0500

    set layout for second VC

--- CustomDelegationLab/CustomDelegationLab/FontChangerController.swift
+++ CustomDelegationLab/CustomDelegationLab/FontChangerController.swift
@@ -9,22 +9,59 @@
 import UIKit
 
 class FontChangerController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBOutlet weak var stepperOutlet: UIStepper!
+    @IBOutlet weak var changeFontLabel: UILabel!
+    
+    var sliderFont: Float? {
+        didSet {
+            updateUI()
+            //changeFontLabel.text = "Preview Font Size: \(Int(sliderFont))"
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureStepper()
+        configureSlide()
+        updateUI()
     }
-    */
-
+    
+    func updateUI(){
+        guard let updatedFontSize = sliderFont else {
+            fatalError("I did not prepare for the segue correctly")
+        }
+        sliderOutlet?.value = updatedFontSize
+        stepperOutlet?.value = Double(updatedFontSize)
+        changeFontLabel?.text = "Preview Font Size: \(Int(updatedFontSize))"
+    }
+    
+    
+    func configureStepper() {
+        stepperOutlet.minimumValue = 5.0
+        stepperOutlet.maximumValue = 50.0
+        stepperOutlet.stepValue = 1.0
+        
+        //stepperOutlet.value = Double(sliderFont) //17.0
+    }
+    
+    func configureSlide() {
+        sliderOutlet.minimumValue = 5
+        sliderOutlet.maximumValue = 50
+        //sliderOutlet.value = sliderFont //17
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        //stepperOutlet.value = Double(sender.value)
+        sliderFont = sender.value
+    }
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        //sliderOutlet.value = Float(sender.value)
+        sliderFont = Float(sender.value) // of type Float
+    }
+    
+    @IBAction func returnButton(_ sender: UIButton) {
+    }
 }
--- CustomDelegationLab/CustomDelegationLab/ViewController.swift
+++ CustomDelegationLab/CustomDelegationLab/ViewController.swift
@@ -9,12 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [Movie]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    var fontSize: Float! {
+        didSet {
+           tableView.reloadData()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        loadData()
     }
+    
+    func loadData() {
+        movies = Movie.allMovies
+        fontSize = Float(17.0)
+    }
+    
 
+@IBAction func changeMovieFont(segue: UIStoryboardSegue) {
+    guard let fontChangingVC = segue.source as? FontChangerController else {
+        fatalError("failed to access CreateEventController")
+    }
+    
+    fontSize = fontChangingVC.sliderFont
 
+}
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let fontChangingVC = segue.destination as? FontChangerController
+            //let indexPath = tableView.indexPathForSelectedRow
+            else {
+                fatalError("failed to get indexPath and fontChangingVC")
+        }
+       //let font = fontSize
+        
+        fontChangingVC.sliderFont = fontSize
+        
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        
+        let movie = movies[indexPath.row]
+        
+        cell.textLabel?.text = movie.name
+        cell.detailTextLabel?.text = movie.year.description
+        cell.textLabel?.font = UIFont(name: "Didot", size: CGFloat(fontSize))
+        cell.detailTextLabel?.font = UIFont(name: "Didot", size: CGFloat(fontSize))
+        return cell
+    }
 }
 
